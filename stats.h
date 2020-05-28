9b039c76b70cc52692924814016b4e9fc3ba06fa
root <you@example.com>
2020-05-28 17:07:12 -0600

    Updated indentation

--- stats.h
+++ stats.h
@@ -28,104 +28,133 @@
 
 int n;
 int num[40];
-int i,j,k;
+int i, j, k;
 int asc[40];
 
-int userarray(){
-printf("You Entered:");
-printf("\n");
-for(j=0;j<n;j++){
-	printf("%d,",num[j]);
-	}
+int
+userarray ()
+{
+  printf ("You Entered:");
+  printf ("\n");
+  for (j = 0; j < n; j++)
+    {
+      printf ("%d,", num[j]);
+    }
 }
 
 /********************Finding Mean*************************/
 
-float mean(){	
-float q=n;
-float sum=0;
-for(i=0;i<n;i++){
-	sum=sum+num[i];
-	}
-float Mean=sum/q;
-printf("mean is:%f",Mean);
-printf("\n");     
+float
+mean ()
+{
+  float q = n;
+  float sum = 0;
+  for (i = 0; i < n; i++)
+    {
+      sum = sum + num[i];
+    }
+  float Mean = sum / q;
+  printf ("mean is:%f", Mean);
+  printf ("\n");
 }
 
-/**********************Finding Median**********************/ 
+/**********************Finding Median**********************/
 
-int median(){
-int temp;                                       
-for(j=0;j<n;j++){                          
-	for(i=0;i<n-1;i++){
-		if(num[i]>num[i+1]){
- 			temp=num[i];        								    				num[i]=num[i+1];       					num[i+1]=temp;    					}                    				}	  	                                              
-								}                                        
-				printf("Ascending Order:");
-					printf("\n"); 
-						for(j=0;j<n;j++){
-									printf("%d,",num[j]);
-											}                                                     
-							printf("\n"); 
-								printf("\n");   
-									int l,m,Median;
-										if(n%2==0){
-													l=n/2;
-															Median=(num[l]+num[l-1])/2;
-																	}
-											else{
-														m=n-1;
-																l=m/2;
-																		Median=num[l];
-																				}
-												printf("Median is:%d",Median); 
-													printf("\n"); 
-														printf("\n");   
+int
+median ()
+{
+  int temp;
+  for (j = 0; j < n; j++)
+    {
+      for (i = 0; i < n - 1; i++)
+	{
+	  if (num[i] > num[i + 1])
+	    {
+	      temp = num[i];
+	      num[i] = num[i + 1];
+	      num[i + 1] = temp;
+	    }
+	}
+    }
+  printf ("Ascending Order:");
+  printf ("\n");
+  for (j = 0; j < n; j++)
+    {
+      printf ("%d,", num[j]);
+    }
+  printf ("\n");
+  printf ("\n");
+  int l, m, Median;
+  if (n % 2 == 0)
+    {
+      l = n / 2;
+      Median = (num[l] + num[l - 1]) / 2;
+    }
+  else
+    {
+      m = n - 1;
+      l = m / 2;
+      Median = num[l];
+    }
+  printf ("Median is:%d", Median);
+  printf ("\n");
+  printf ("\n");
 }
 
 /**************Sorting in Descending Order********************/
 
-int sort(){
-		int desc[40];
-			for(i=0;i<n;i++){
-						desc[i]=num[n-1-i];
-								}
-				printf("Descending Order:");
-					printf("\n"); 
-						for(j=0;j<n;j++){
-									printf("%d,",desc[j]);
-											}                                                                       
-							printf("\n"); 
-								printf("\n");   
+int
+sort ()
+{
+  int desc[40];
+  for (i = 0; i < n; i++)
+    {
+      desc[i] = num[n - 1 - i];
+    }
+  printf ("Descending Order:");
+  printf ("\n");
+  for (j = 0; j < n; j++)
+    {
+      printf ("%d,", desc[j]);
+    }
+  printf ("\n");
+  printf ("\n");
 }
 
-/*****************Finding Minimum and Maximum***************/                   
+/*****************Finding Minimum and Maximum***************/
 
-int maximum(){
-		int max=num[0];
-			for(i=0;i<n;i++){
-						if(num[i]>max){
-										max=num[i];
-													}
-								}
-				printf("\n");
-					printf("Largest number is %d",max);
-						printf("\n");
-							}
+int
+maximum ()
+{
+  int max = num[0];
+  for (i = 0; i < n; i++)
+    {
+      if (num[i] > max)
+	{
+	  max = num[i];
+	}
+    }
+  printf ("\n");
+  printf ("Largest number is %d", max);
+  printf ("\n");
+}
 
-int minimum(){
-		int min=num[0];
-			for(i=0;i<n;i++){
-						if(num[i]<min){
-										min=num[i];
-													}
-								}
-				printf("\n");	
-					printf("Smallest number is %d",min);
-						printf("\n");  
-							}
+int
+minimum ()
+{
+  int min = num[0];
+  for (i = 0; i < n; i++)
+    {
+      if (num[i] < min)
+	{
+	  min = num[i];
+	}
+    }
+  printf ("\n");
+  printf ("Smallest number is %d", min);
+  printf ("\n");
+}
 
 #endif
 
-		/*End*/
-		
+		/*End */
