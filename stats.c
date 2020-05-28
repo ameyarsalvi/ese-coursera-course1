fc012126949de536ed92edea7c3940f48e34db9b
root <you@example.com>
2020-05-28 16:19:58 -0600

    Updated Files with required code and functions_First Update

--- stats.c
+++ stats.c
@@ -9,35 +9,66 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <This is the main file for the first assignemrnt >
  *
- * <Add Extended Description Here>
+ * <The execution of this files provide statistics for the array provided in the assignment detials. After that, the program asks user to input array of their choice and provides stastics on that array>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Ameya Salvi>
+ * @date <May 28,2020 >
  *
- */
+*/
+
+#include "Header.h"
+int n=40;
+
+void main(){
+	int test[] = { 34, 201, 190, 154,   8, 194,   2,   6,
+		       114, 88,   45,  76, 123,  87,  25,  23,
+		       200, 122, 150, 90,   92,  87, 177, 244,
+		       201,   6,  12,  60,   8,   2,   5,  67,
+	               7,  87, 250, 230,  99,   3, 100,  90};
+
+for(i=0;i<n;i++){
+	num[i]=test[i];
+	}
+
+mean();
+printf("\n");
+median();
+printf("\n");
+sort();
+printf("\n");
+maximum();
+printf("\n");
+minimum();
 
 
+printf("\n");
+printf("How Many numbers you want to enter? You are allowed upto 40 numbers\n");
+scanf("%d",&n);
+printf("Enter those %d numbers\n",n);
+for(k=0;k<n;k++){
+	scanf("%d",&num[k]);
+	}
+userarray();
+printf("\n");
 
-#include <stdio.h>
-#include "stats.h"
+printf("Statistics for the provided array are \n");
 
-/* Size of the Data Set */
-#define SIZE (40)
+mean();
+printf("\n");
+median();
+printf("\n");
+sort();
+printf("\n");
+maximum();
+printf("\n");
+minimum();
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
 
 }
 
-/* Add other Implementation File Code Here */
+
+
+
--- stats.h
+++ stats.h
@@ -18,24 +18,114 @@
  * @date <May 10 2020 >
  *
  */
-#ifndef __STATS_H__
-#define __STATS_H__
-
-/* Add Your Declarations and Function Comments here */ 
-
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
 
 
-#endif /* __STATS_H__ */
+#ifndef HEADER_H
+#define HEADER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+int num[40];
+int i,j,k;
+int asc[40];
+
+int userarray(){
+printf("You Entered:");
+printf("\n");
+for(j=0;j<n;j++){
+	printf("%d,",num[j]);
+	}
+}
+
+/********************Finding Mean*************************/
+
+float mean(){	
+float q=n;
+float sum=0;
+for(i=0;i<n;i++){
+	sum=sum+num[i];
+	}
+float Mean=sum/q;
+printf("mean is:%f",Mean);
+printf("\n");     
+}
+
+/**********************Finding Median**********************/ 
+
+int median(){
+int temp;                                       
+for(j=0;j<n;j++){                          
+	for(i=0;i<n-1;i++){
+		if(num[i]>num[i+1]){
+ 			temp=num[i];        								    				num[i]=num[i+1];       					num[i+1]=temp;    					}                    				}	  	                                              
+								}                                        
+				printf("Ascending Order:");
+					printf("\n"); 
+						for(j=0;j<n;j++){
+									printf("%d,",num[j]);
+											}                                                     
+							printf("\n"); 
+								printf("\n");   
+									int l,m,Median;
+										if(n%2==0){
+													l=n/2;
+															Median=(num[l]+num[l-1])/2;
+																	}
+											else{
+														m=n-1;
+																l=m/2;
+																		Median=num[l];
+																				}
+												printf("Median is:%d",Median); 
+													printf("\n"); 
+														printf("\n");   
+}
+
+/**************Sorting in Descending Order********************/
+
+int sort(){
+		int desc[40];
+			for(i=0;i<n;i++){
+						desc[i]=num[n-1-i];
+								}
+				printf("Descending Order:");
+					printf("\n"); 
+						for(j=0;j<n;j++){
+									printf("%d,",desc[j]);
+											}                                                                       
+							printf("\n"); 
+								printf("\n");   
+}
+
+/*****************Finding Minimum and Maximum***************/                   
+
+int maximum(){
+		int max=num[0];
+			for(i=0;i<n;i++){
+						if(num[i]>max){
+										max=num[i];
+													}
+								}
+				printf("\n");
+					printf("Largest number is %d",max);
+						printf("\n");
+							}
+
+int minimum(){
+		int min=num[0];
+			for(i=0;i<n;i++){
+						if(num[i]<min){
+										min=num[i];
+													}
+								}
+				printf("\n");	
+					printf("Smallest number is %d",min);
+						printf("\n");  
+							}
+
+#endif
+
+		/*End*/
+		
